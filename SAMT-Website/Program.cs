d1c0d89323afdf22d8e21a6b41d3a4c2424d4bcf
MarkenJaden <jjsch1410@gmail.com>
2024-01-31 18:39:58 +0100

    Try to fix redirecting

--- SAMT-Website/Program.cs
+++ SAMT-Website/Program.cs
@@ -20,6 +20,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.Urls.Add("http://localhost:5000");
 }
 
 app.UseHttpsRedirection();
